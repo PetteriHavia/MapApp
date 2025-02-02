e44a162e590581677c23bc29187b340b53671253
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2025-02-02 23:14:09 +0200

    feat: Map will now move to the marker location

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import "./App.css";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Controls from "./components/Controls";
 import { Coords } from "./types";
 
 function App() {
   const [userLocation, setUserLocation] = useState<Coords>();
+  const mapRef = useRef<L.Map | null>(null);
+
+  useEffect(() => {
+    if (userLocation && mapRef.current) {
+      mapRef.current.flyTo([userLocation.latitude, userLocation.longitude], 13);
+    }
+  }, [userLocation]);
 
   return (
     <div className="w-full h-dvh flex justify-center items-center flex-col">
       <Controls setUserLocation={setUserLocation} />
-      <MapContainer center={[51.505, -0.09]} zoom={13} className="w-9/10 h-[800px]">
+      <MapContainer center={[51.505, -0.09]} zoom={13} className="w-9/10 h-[800px]" ref={mapRef}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
