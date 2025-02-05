0696ea02d0c43595df0cc8ec704c736e2f2b1996
Petteri Havia <petsku15@hotmail.fi>
2025-02-05 20:09:55 +0200

    refactor: change state "userLocation" to "location". location will now be used by other components to set coords

--- src/components/MapFlyTo.tsx
+++ src/components/MapFlyTo.tsx
@@ -2,13 +2,13 @@ import { useEffect } from "react";
 import { Coords } from "../types";
 import { useMap } from "react-leaflet";
 
-const MapFlyTo = ({ userLocation }: { userLocation?: Coords }) => {
+const MapFlyTo = ({ location }: { location?: Coords }) => {
   const map = useMap();
   useEffect(() => {
-    if (userLocation) {
-      map.flyTo([userLocation.latitude, userLocation.longitude], 13);
+    if (location) {
+      map.flyTo([location.latitude, location.longitude], 13);
     }
-  }, [userLocation, map]);
+  }, [location, map]);
   return null;
 };
 
