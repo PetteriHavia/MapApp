2c039fbe48b6872bc93b490fb844df581130924b
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2025-02-08 18:25:20 +0200

    refactor: combine params and URLSearchParams for more compact form

--- src/services/nominativeService.tsx
+++ src/services/nominativeService.tsx
@@ -1,7 +1,14 @@
+import { Coords } from "../types";
+
 const BASE_URL = "https://nominatim.openstreetmap.org";
 
-export const getAddress = async (address: string) => {
-  const response = await fetch(`${BASE_URL}/search?${address}`);
-  const data = await response.json();
+export const getAddress = async (address: string): Promise<Coords | null> => {
+  const params = new URLSearchParams({
+    q: address,
+    limit: "1",
+    format: "json",
+  });
+  const response = await fetch(`${BASE_URL}/search?${params.toString()}`);
+  const data: Coords[] = await response.json();
   return data.length > 0 ? data[0] : null;
 };
