c42574baeff7b33d3069bf6dbada3f13a7b2e43f
Petteri Havia <petsku15@hotmail.fi>
2025-02-10 23:36:48 +0200

    feat: add toggle state that renders alternative route for waypoints

--- src/components/RoutingMachine.tsx
+++ src/components/RoutingMachine.tsx
@@ -1,31 +1,47 @@
 import L from "leaflet";
-import { createControlComponent } from "@react-leaflet/core";
+import { useEffect } from "react";
 import "leaflet-routing-machine";
 import { RouteCoords } from "../types";
+import { useMap } from "react-leaflet";
 
 type Props = {
   route: RouteCoords;
+  altRoute: boolean;
 };
 
-const createRoutineMachineLayer = (props: any) => {
-  const { route }: Props = props;
-  const instance = L.Routing.control({
-    waypoints: [L.latLng(route.start.lat, route.start.lon), L.latLng(route.end.lat, route.end.lon)],
-    lineOptions: {
-      styles: [{ color: "#6FA1EC", weight: 4 }],
-    },
-    show: true,
-    addWaypoints: false,
-    routeWhileDragging: true,
-    draggableWaypoints: true,
-    fitSelectedRoutes: true,
-    showAlternatives: false,
-    collapsible: true,
-  });
-  console.log(instance.waypoints);
-  return instance;
-};
+const RoutingMachine = ({ altRoute, route }: Props) => {
+  const map = useMap();
 
-const RoutingMachine = createControlComponent(createRoutineMachineLayer);
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
+
+    const routingControl = L.Routing.control({
+      waypoints: [L.latLng(route.start.lat, route.start.lon), L.latLng(route.end.lat, route.end.lon)],
+      lineOptions: {
+        styles: [{ color: "#6FA1EC", weight: 4 }],
+      },
+      altLineOptions: {
+        styles: [{ color: "#1c4ae0", weight: 4 }],
+      },
+      show: true,
+      autoRoute: false,
+      addWaypoints: false,
+      draggableWaypoints: true,
+      fitSelectedRoutes: true,
+      showAlternatives: altRoute,
+      collapsible: true,
+    });
+
+    routingControl.addTo(map);
+
+    return () => {
+      map.removeControl(routingControl);
+    };
+  }, [map, route, altRoute]);
+
+  return null;
+};
 
 export default RoutingMachine;
