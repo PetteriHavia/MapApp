2ac7b7e420b5ad9815c9308968a223f816bed32c
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2025-02-12 02:46:37 +0200

    styling: style app + create re-usable button utility class

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,23 @@ import "./App.css";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 import { useState } from "react";
-import Controls from "./components/Controls";
+import Controls from "./components/Controls/Controls";
 import { Coords, RouteCoords } from "./types";
 import LayerControlComponent from "./components/LayerControl";
 import MapFlyTo from "./components/MapFlyTo";
-import MapGeolocation from "./components/MapGeolocation";
 import RoutingMachine from "./components/RoutingMachine";
-//import RoutingMachine from "./components/RoutingMachine";
 
 function App() {
   const [location, setLocation] = useState<Coords>();
   const [route, setRoute] = useState<RouteCoords>();
   const [altRoute, setAltRoute] = useState<boolean>(false);
-  console.log(route);
+
   return (
-    <div className="w-full h-dvh flex justify-center items-center flex-col">
-      <MapGeolocation setLocation={setLocation} />
-      <Controls setLocation={setLocation} setRoute={setRoute} setAltRoute={setAltRoute} altRoute={altRoute} />
-      <MapContainer center={[51.505, -0.09]} zoom={13} className="w-9/10 h-[800px]">
+    <div className="flex">
+      <div className="w-full max-w-sm -xs p-5">
+        <Controls setLocation={setLocation} setRoute={setRoute} setAltRoute={setAltRoute} />
+      </div>
+      <MapContainer center={[51.505, -0.09]} zoom={13} className="w-full h-full h-screen">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
