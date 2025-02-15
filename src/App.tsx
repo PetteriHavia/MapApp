b607e6b1941494e5e221f0af83c6be89a2f33e86
Petteri Havia <petsku15@hotmail.fi>
2025-02-15 19:23:40 +0200

    styling: implement a collapsible sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
 import "./App.css";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import { MapContainer } from "react-leaflet";
 import { useState } from "react";
 import Controls from "./components/Controls/Controls";
 import { Coords, RouteCoords } from "./types";
-import LayerControlComponent from "./components/LayerControl";
-import MapFlyTo from "./components/MapFlyTo";
-import RoutingMachine from "./components/RoutingMachine";
+import MapWrapper from "./components/MapWrapper";
 
 function App() {
   const [location, setLocation] = useState<Coords>();
   const [route, setRoute] = useState<RouteCoords>();
   const [altRoute, setAltRoute] = useState<boolean>(false);
+  const [drawerToggle, setDrawerToggle] = useState<boolean>(false);
 
   return (
-    <div className="flex">
-      <div className="w-full max-w-sm -xs p-5">
-        <Controls setLocation={setLocation} setRoute={setRoute} setAltRoute={setAltRoute} />
+    <div className="flex relative">
+      <div
+        className={`absolute left-20 bg-white z-2 p-2 mt-[10px] rounded-sm btn-primary ${drawerToggle ? "" : "hidden"}`}
+      >
+        <button onClick={() => setDrawerToggle((prev) => !prev)}>OPEN NAVIGATION</button>
       </div>
-      <MapContainer center={[51.505, -0.09]} zoom={13} className="w-full h-full h-screen">
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      <div
+        className={`transition-all duration-300 z-3 ${
+          drawerToggle ? "-translate-x-100 w-0 p-0 overflow-hidden" : "w-110 p-5"
+        } bg-white`}
+      >
+        <Controls
+          setLocation={setLocation}
+          setRoute={setRoute}
+          setAltRoute={setAltRoute}
+          setDrawerToggle={setDrawerToggle}
+          drawerToggle={drawerToggle}
         />
-        <LayerControlComponent />
-        <MapFlyTo location={location} />
-        {route && <RoutingMachine route={route} altRoute={altRoute} />}
-        {location && (
-          <Marker position={[location.lat, location.lon]}>
-            <Popup>Here i am</Popup>
-          </Marker>
-        )}
+      </div>
+      <MapContainer center={[51.505, -0.09]} zoom={13} className="w-full h-full h-screen z-1">
+        <MapWrapper location={location} route={route} altRoute={altRoute} drawerToggle={drawerToggle} />
       </MapContainer>
     </div>
   );
