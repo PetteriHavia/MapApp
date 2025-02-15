8cf99839dd155e44d14d5793b7f745bf49e8085b
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2025-02-15 19:29:17 +0200

    fix: remove prop

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,6 @@ function App() {
           setRoute={setRoute}
           setAltRoute={setAltRoute}
           setDrawerToggle={setDrawerToggle}
-          drawerToggle={drawerToggle}
         />
       </div>
       <MapContainer center={[51.505, -0.09]} zoom={13} className="w-full h-full h-screen z-1">
