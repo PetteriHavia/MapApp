e80734329fa411c7887793663e8f13800bc2fc1e
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2025-03-04 18:03:52 +0200

    style: minor style changes for routing machine default styling + added popups for waypoint markers

--- src/components/RoutingMachine.tsx
+++ src/components/RoutingMachine.tsx
@@ -20,10 +20,22 @@ const RoutingMachine = ({ altRoute, route }: Props) => {
     const routingControl = L.Routing.control({
       waypoints: [L.latLng(route.start.lat, route.start.lon), L.latLng(route.end.lat, route.end.lon)],
       lineOptions: {
-        styles: [{ color: "#6FA1EC", weight: 4 }],
+        styles: [{ color: "#1c4ae0", weight: 4 }],
       },
       altLineOptions: {
-        styles: [{ color: "#1c4ae0", weight: 4 }],
+        styles: [{ color: "#6fa1ec", weight: 4 }],
+      },
+      createMarker: function (i: number, waypoint, n: number) {
+        let popupContent = "Waypoint " + (i + 1);
+        if (i === 0) {
+          popupContent = "Route Start";
+        } else if (i === n - 1) {
+          popupContent = "Route End";
+        }
+        return L.marker(waypoint.latLng).bindPopup(popupContent);
+      },
+      pointMarkerStyle: {
+        color: "#ee4f4f",
       },
       show: true,
       autoRoute: true,
@@ -37,7 +49,9 @@ const RoutingMachine = ({ altRoute, route }: Props) => {
     routingControl.addTo(map);
 
     return () => {
-      map.removeControl(routingControl);
+      if (map) {
+        map.removeControl(routingControl);
+      }
     };
   }, [map, route, altRoute]);
 
